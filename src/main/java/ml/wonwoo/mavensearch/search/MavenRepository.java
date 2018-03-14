8e5a40764d26b84d1489f28c542e3cdd62fb4d9d
wonwoo <aoruqjfu@gmail.com>
2018-03-14 20:31:24 +0900

    MeterRegistry or cache delete

--- src/main/java/ml/wonwoo/mavensearch/search/MavenRepository.java
+++ src/main/java/ml/wonwoo/mavensearch/search/MavenRepository.java
@@ -1,10 +1,10 @@
 package ml.wonwoo.mavensearch.search;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import ml.wonwoo.mavensearch.search.model.Docs;
 import ml.wonwoo.mavensearch.search.model.Maven;
 import ml.wonwoo.mavensearch.search.model.VersionDocs;
@@ -15,23 +15,24 @@ public class MavenRepository {
 
   private static final String MAVEN_BASE_URL = "http://search.maven.org";
   private final WebClient webClient;
+  private final MeterRegistry meterRegistry;
 
-  public MavenRepository(WebClient.Builder builder) {
+  public MavenRepository(WebClient.Builder builder, MeterRegistry meterRegistry) {
     this.webClient = builder
         .baseUrl(MAVEN_BASE_URL)
         .build();
+    this.meterRegistry = meterRegistry;
   }
 
-  @Cacheable("select")
   public Mono<Maven<Docs>> select(String q, int row, int start) {
     return this.webClient
             .get()
             .uri("/solrsearch/select?q=\"{q}\"&rows={row}&wt=json&start={start}", q, row, start)
             .retrieve()
-            .bodyToMono(new ParameterizedTypeReference<Maven<Docs>>() {});
+            .bodyToMono(new ParameterizedTypeReference<Maven<Docs>>() {})
+            .doFinally(signalType -> increment("maven.versions"));
   }
 
-  @Cacheable("versions")
   public Mono<Maven<VersionDocs>> versions(String g, String a, int row, int start) {
     String url = String.format("/solrsearch/select?q=g:%s+AND+a:%s&rows=%s&wt=json&start=%s&core=gav",
         g, a, row, start);
@@ -39,7 +40,11 @@ public class MavenRepository {
         .get()
         .uri(url)
         .retrieve()
-        .bodyToMono(new ParameterizedTypeReference<Maven<VersionDocs>>() {});
+        .bodyToMono(new ParameterizedTypeReference<Maven<VersionDocs>>() {})
+        .doFinally(signalType -> increment("maven.versions"));
   }
 
+  private void increment(String key) {
+    meterRegistry.counter(key).increment();
+  }
 }
